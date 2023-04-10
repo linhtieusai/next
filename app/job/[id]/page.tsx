ecf0dddce79235672fb82a017068e11e4fc9be1e
Le Dinh Linh <le_dinh_linh@mut.co.jp>
2023-04-10 10:29:52 +0900

    generate static for Job Detail

--- app/job/[id]/page.tsx
+++ app/job/[id]/page.tsx
@@ -1,4 +1,6 @@
-async function getData(id: number) {
+import { Suspense } from "react";
+
+async function getData(id: string) {
   const res = await fetch(`http://localhost:3000/api/job/${id}`);
 
 
@@ -14,13 +16,30 @@ async function getData(id: number) {
   return await res.json();
 }
 
-export default async function Page({ params }) {
-  const data = await getData(params.id);
-  console.log(data);
+export async function generateStaticParams() {
+  const posts = await fetch('http://localhost:3000/api/jobs').then((res) => res.json());
+
+  return posts.map((post) => ({
+    id: post.id.toString(),
+  }));
+}
+
+
+export default async function Page({ params }: { params: { id: string } }) {
+  const { id } = params;
+  const data = await getData(id);
   return (
     <main>
-      <h1>job title 2</h1>
-      <h1>{data.title}</h1>
+
+      <Suspense fallback={<p>Loading feed...</p>}>
+        <h1>job title 2</h1>
+        <h1>{data.title}</h1>
+        <p>{data.why_should_apply}</p>
+        <p>{data.job_required_skill}</p>
+        <h3>{data.company_address}</h3>
+        <p>{data.company_description}</p>
+      </Suspense>
+      
     </main>
   );
 }
