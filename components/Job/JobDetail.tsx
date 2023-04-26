ef9a3726e72ca2d55015440badf91406302a9b09
Le Dinh Linh <le_dinh_linh@mut.co.jp>
2023-04-26 14:13:28 +0900

    sticky job header va job content

--- components/Job/JobDetail.tsx
+++ components/Job/JobDetail.tsx
@@ -20,11 +20,11 @@ function JobDetail({ selectedJob, handleBackButton, handleApplyButtonClick }) {
             >
                 Back to Search Results for "PHP"
             </a>
-            <div className="sticky top-0 flex-col items-center justify-between px-5 py-10 h-[calc(100vh_-_100px)] overflow-auto bg-white border-b">
-                <div className="flex justify-between">
-                  <h1 className="flex-none max-w-full ml-2 text-2xl font-bold overflow-ellipsis clamp-2">{selectedJob.title}</h1>
+            <div className="sticky top-0 flex-col items-center justify-between px-5 pb-10 h-[calc(100vh_-_100px)] overflow-auto bg-white border-b">
+                <div className="sticky top-0 flex justify-between py-3 bg-white">
+                  <h1 className="flex ml-2 text-2xl font-bold overflow-ellipsis clamp-2">{selectedJob.title}</h1>
                   <div className="flex items-center gap-2">
-                    <button className="px-4 py-2 font-bold text-white bg-red-500 rounded-full hover:bg-red-600">
+                    <button className="flex items-center px-4 py-2 font-bold text-white bg-red-500 rounded-full hover:bg-red-600">
                       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="inline-block w-4 h-4">
                         <path d="M20.84 4.76a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-.9a5.5 5.5 0 0 0-7.78 7.78L12 21l8.84-8.84a5.5 5.5 0 0 0 0-7.78z"></path>
                       </svg>
@@ -36,7 +36,7 @@ function JobDetail({ selectedJob, handleBackButton, handleApplyButtonClick }) {
                   </div>
                 </div>
                
-                <div className="pt-16 pl-5">
+                <div className="pl-5 pt-13">
                     <h3 className="mt-3 text-lg font-bold">Required Skill</h3>
                     <p className="pl-4 whitespace-pre-wrap">{selectedJob?.job_required_skill}</p>
                     {/* <h3 className="text-lg font-bold">Job overview</h3>
