75a2e227a7040a93879db603c62fb82cd424e449
Le Dinh Linh <le_dinh_linh@mut.co.jp>
2023-05-16 18:26:59 +0900

    select latest candidate for aplication

--- components/JobDetail/ApplyScreen.tsx
+++ components/JobDetail/ApplyScreen.tsx
@@ -81,6 +81,16 @@ const ApplyButton = ({jobId, presubmitInfo, isModalOpening, closeModalCallBack})
 
   const handleCandidateSelectButton = () => {
     setIsSelectCandidateOpen(true);
+
+    fetch(`http://localhost:3000/api/applications`)
+      .then(response => response.json())
+      .then(data =>  {
+
+          setLatestCandidateList(data.candidates);
+          
+        }).catch(error => {
+          console.error(error)
+        });
   }
 
   const  handleFirstStepSubmit = async (event) => {
@@ -546,37 +556,36 @@ const ApplyButton = ({jobId, presubmitInfo, isModalOpening, closeModalCallBack})
       {isSelectCandidateOpen && (
         <Modal zIndex={6} onClose={handleCandidateSelectBack} showCloseButton>
           <div className="flex">
-            {latestCandidateList && (
+            {latestCandidateList ? (
               <>
               ddsdsd
               </>
-            )}
-            {true && (
-                        <div className="absolute top-0 left-0 flex items-center justify-center w-full h-full">
-                          <div className="absolute top-0 left-0 z-10 w-full h-full bg-black opacity-20"></div>
-                          <div className="z-20">
-                            <svg
-                              className="w-8 h-8 mx-auto text-white animate-spin"
-                              xmlns="http://www.w3.org/2000/svg"
-                              fill="none"
-                              viewBox="0 0 24 24"
-                            >
-                              <circle
-                                className="opacity-25"
-                                cx="12"
-                                cy="12"
-                                r="10"
-                                stroke="currentColor"
-                                strokeWidth="4"
-                              ></circle>
-                              <path
-                                className="opacity-75"
-                                fill="currentColor"
-                                d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-                              ></path>
-                            </svg>
-                          </div>
-                        </div>
+            ) : (
+                    <div className="absolute top-0 left-0 flex items-center justify-center w-full h-full">
+                      <div className="absolute top-0 left-0 z-10 w-full h-full bg-black opacity-20"></div>
+                      <div className="z-20">
+                        <svg
+                          className="w-8 h-8 mx-auto text-white animate-spin"
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                        >
+                          <circle
+                            className="opacity-25"
+                            cx="12"
+                            cy="12"
+                            r="10"
+                            stroke="currentColor"
+                            strokeWidth="4"
+                          ></circle>
+                          <path
+                            className="opacity-75"
+                            fill="currentColor"
+                            d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                          ></path>
+                        </svg>
+                      </div>
+                    </div>
                   )}
             </div>
           
