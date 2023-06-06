d71a01662d8d4e24c169113f865b9677d63d2def
Le Dinh Linh <le_dinh_linh@mut.co.jp>
2023-06-06 11:12:39 +0900

    fix Viewed Job page load from API for logged user

--- app/viewed/page.tsx
+++ app/viewed/page.tsx
@@ -52,7 +52,7 @@ export default function ViewedJobPage({ searchParams }) {
 
 
   
-function getViewedJobs(page) {
+async function getViewedJobs(page) {
   if(session) {
     return getViewedJobFromServer(page);
   } else {
@@ -63,10 +63,14 @@ function getViewedJobs(page) {
 async function getViewedJobFromLocalStorage(page) {
   const perPage = 10;
   let jobViewHistory = JSON.parse(localStorage.getItem("jobViewHistory") ?? "[]");
+  
+  console.log("load from local storage");
 
   jobViewHistory = jobViewHistory.reverse();
 
   if(!page) page = 1;
+
+
 
   console.log(Math.ceil(jobViewHistory.length / perPage));
   return {
@@ -78,27 +82,15 @@ async function getViewedJobFromLocalStorage(page) {
 
 async function getViewedJobFromServer(page) {
   const perPage = 10;
-  // let jobViewHistory = JSON.parse(localStorage.getItem("jobViewHistory") ?? "[]");
-
-  // jobViewHistory = jobViewHistory.reverse();
 
   if(!page) page = 1;
 
-  // console.log(Math.ceil(jobViewHistory.length / perPage));
-  return {
-    jobs: jobViewHistory.slice((page - 1) * perPage, page * perPage) ?? [],
-    totalPages: Math.ceil(jobViewHistory.length / perPage),
-    page: page
-  }
+  console.log("load api");
 
-  if(!page) page = 1;
+  const data = await fetch(`http://localhost:3000/api/viewedJobs?page=${page}`);
 
-  fetch(`http://localhost:3000/api/viewedJobs?page=${page}`)
-      .then(response => response.json())
-      .then(data => {
-        // setPresubmitInfo(data);
-      })
-      .catch(error => console.error(error));
+  console.log(data);
+  return data.json();
 }
 
 
@@ -156,35 +148,40 @@ async function getViewedJobFromServer(page) {
     setCurrentPage(value);
   }
 
+  const getJobData = (jobData) => {
+    if(session) {
+      let viewedData = {};
+      for (const job of jobData) {
+        viewedData[job.job_id] = job.job;
+      }
+
+      return viewedData;
+    } else {
+      return JSON.parse(localStorage.getItem("viewedJobs") ?? "[]");
+    }
+  }
+
   useEffect(() => {
-    // if(page > 1) {
-    //   fetch(`http://localhost:3000/api/jobs?page=${page}&itemsPerPage=10`)
-    //   .then(response => response.json())
-    //   .then(data => setJobs(data.jobs))
-    //   .catch(error => console.error(error));
-
-    //   setJobs(getViewedJobFromLocalStorage(page));
-    // }
-
     console.log("go use effect");
-    // if(page != currentPage) {
-      console.log("go here");
-      getViewedJobs(currentPage)
-      .then(data => {
-        setJobViewHistory(data.jobs);
+    getViewedJobs(currentPage)
+    .then(data => {
 
-        const jobData = JSON.parse(localStorage.getItem("viewedJobs") ?? "[]");
-        setJobData(jobData);
+      console.log(data);
+      setJobViewHistory(data.jobs);
 
-        callBackMethod(data.totalPages, data.page);
-        const followedJobs = JSON.parse(localStorage.getItem("followedJobs") ?? "[]");
-        setFollowedJobs(followedJobs);
-      });
-  // }
+      const jobData = getJobData(data.jobs);
+      console.log("jobData");
+      console.log(jobData);
+      setJobData(jobData);
+
+      console.log(JSON.parse(localStorage.getItem("viewedJobs") ?? "[]"));
+
+      callBackMethod(data.totalPages, data.page);
+      const followedJobs = JSON.parse(localStorage.getItem("followedJobs") ?? "[]");
+      setFollowedJobs(followedJobs);
+    });
     
-    //setJobs(await getViewedJobFromLocalStorage(page));
-
-  }, [currentPage]);
+  }, [page]);
 
 
   return (
@@ -217,40 +214,7 @@ async function getViewedJobFromServer(page) {
               
              : <JobListSkeleton />
             }
-              
-
-            {/* {isMoving && (
-                <JobListSkeleton />
-            )} */}
         </ul>
-
-        {/* {isMoving && (
-          <div className="absolute top-0 left-0 z-1 items-center flex justify-center w-full h-full">
-            <div className="absolute top-0 left-0 z-1 w-full h-full bg-black opacity-20"></div>
-            <div className="z-2">
-              <svg
-                className="w-8 h-8 mx-auto text-white animate-spin"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-              >
-                <circle
-                  className="opacity-25"
-                  cx="12"
-                  cy="12"
-                  r="10"
-                  stroke="currentColor"
-                  strokeWidth="4"
-                ></circle>
-                <path
-                  className="opacity-75"
-                  fill="currentColor"
-                  d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-                ></path>
-              </svg>
-            </div>
-          </div>
-        )} */}
 
         <div className="sticky bottom-0 z-3 bg-white flex items-center justify-center w-full py-2 md:py-4">
           {totalPages > 1 && currentPage && (
